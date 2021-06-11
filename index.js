aee54d55705d22c9c4275936470e4d5ecc70999a
Lee Scott <Odda@MacBook-Pro-2.local>
2021-06-11 17:43:34 -0400

    passed 5 of 8 tests for js portion

--- index.js
+++ index.js
@@ -208,16 +208,17 @@ Practice accessing data above by console.log-ing following items:
 (no functions needed) */
 
 //(1) Name of the first artist (0th index) in the array
-
+console.log(artists[0].name)
 
 //(2) Bio of the third artist (2nd index) in the array 
-
+console.log(artists[2].bio)
 
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 2: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 
 (no function needed) 
 There is a typo in your dataset 😱 The 9th artist, Vincent Van Gogh is currently Vincent Van Dough. Use an array method to fix this issue and console.log() to check your work. */
-
+artists[8].name = "Vincent Van Gogh"
+// console.log(artists)
 
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 3: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀  
@@ -228,8 +229,8 @@ There is a typo in your dataset 😱 The 9th artist, Vincent Van Gogh is current
  
  Example, if getArtistByIndex is invoked with the artists array and the number 0, it will return `the artist at index 0 is Amedeo Modigliani` */
 
-function getArtistByIndex(/*Your Code Here*/) {
-  /*Your Code Here*/
+function getArtistByIndex(arr, index) {
+  return 'the artist at index ' + index + ' is ' + arr[index].name;
 }  
 
 
@@ -243,9 +244,23 @@ Example born in 1901 and died in 1959 - included -- born in 1889 and died in 192
 If correct, the function should return ["Salvador Dali", "Frida Kahlo"]*/
 // Hint - Look up the .split() method
 
-function get20s(/*Your Code Here*/){
-  /*Your Code Here*/
+function get20s(array){
+  let newArr = []
+  let century = []
+  for (let i = 1900; i<2000; i++){
+    century.push(i + '')
+  }console.log(century)
+  for (let j = 0; j<array.length; j++){
+    if (century.includes(array[j].years.split(' ')[0]) && century.includes(array[j].years.split(' ')[2])){
+      newArr.push(array[j].name)
+    } return newArr;
+  }console.log(newArr)
 }
+
+
+ 
+  
+
 
 
 
@@ -258,8 +273,10 @@ function get20s(/*Your Code Here*/){
  
  For example, if removeArtist is invoked with the artists array and the number 0, it will remove Amedeo Modigliani from our dataset and return the number 19. */
 
-function removeArtist(/*Your Code Here*/){
-   /*Your Code Here*/
+function removeArtist(array, i){
+   array.splice(i, 1)
+   return array.length
+
 }
    
 
